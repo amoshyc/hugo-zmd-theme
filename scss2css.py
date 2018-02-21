739cb72863fa64bb28e5670a26fe2751bcdc1677
amoshyc <amoshuangyc@gmail.com>
2018-02-21 15:21:07 +0800

    tune style

--- scss2css.py
+++ scss2css.py
@@ -2,6 +2,5 @@ import sass
 
 if __name__ == '__main__':
     res = sass.compile(filename='scss/style.scss', output_style='compressed')
-    # res = sass.compile(filename='scss/style.scss')
     with open('static/style.min.css', 'w') as f:
         f.write(res)
